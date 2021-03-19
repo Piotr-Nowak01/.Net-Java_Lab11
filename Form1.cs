ba8664a8b7f02d062be24a3a117965a6f17c92f2
Piotr Nowak <62243821+Piotr-Nowak01@users.noreply.github.com>
2021-03-19 12:43:59 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConsoleApp1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Aplikacja : Form
+    {
+        public Aplikacja()
+        {
+            InitializeComponent();
+        }
+        struct przedmiot
+        {
+            public int waga;
+            public int wartosc;
+        };
+        private void button1_Click(object sender, EventArgs e)
+        {
+               
+            int n = 20, down = 1, up = 29;
+            int max_pojemnosc = 200, curr_pojemnosc = 0;
+            int wartosc = 0, iter = 0;
+            przedmiot[] itemy = new przedmiot[n];
+            RandomNumberGenerator nazwa = new RandomNumberGenerator(2137);
+            for (int i = 0; i < n; i++)
+            {
+                itemy[i].waga = nazwa.nextInt(down, up);
+                itemy[i].wartosc = nazwa.nextInt(down, up);
+            }
+            while (curr_pojemnosc + itemy[iter].waga < max_pojemnosc)
+            {
+                curr_pojemnosc += itemy[iter].waga;
+                wartosc += itemy[iter].wartosc;
+                iter++;
+            }
+            string text = "Wartosc itemow: ";
+            text += wartosc.ToString() + "\n";
+            text += " Zajeta pojemnosc: ";
+            text += curr_pojemnosc.ToString() + "\n";
+            textBox1.Text = text;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,36 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-namespace ConsoleApp1
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
 {
-    public class Program
+    static class Program
     {
-        struct przedmiot
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            public int waga;
-            public int wartosc;
-        }
-        static void Main(string[] args)
-        {
-            int n = 20, down = 1, up = 29;
-            int max_pojemnosc = 200, curr_pojemnosc = 0;
-            int wartosc = 0, iter = 0;
-            przedmiot[] itemy = new przedmiot[n]; 
-            RandomNumberGenerator nazwa = new RandomNumberGenerator(2137);
-            for(int i=0; i<n;i++)
-            {
-                itemy[i].waga = nazwa.nextInt(down,up);
-                itemy[i].wartosc = nazwa.nextInt(down,up);
-            }
-            while(curr_pojemnosc + itemy[iter].waga < max_pojemnosc)
-            {
-                curr_pojemnosc += itemy[iter].waga;
-                wartosc += itemy[iter].wartosc;
-                iter++;
-            }
-            Console.WriteLine("Wartość itemów = {0}. Obecna waga = {1}", wartosc, curr_pojemnosc);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Aplikacja());
         }
     }
 }
